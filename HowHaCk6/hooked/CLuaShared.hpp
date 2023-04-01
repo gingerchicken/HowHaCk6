560233abd7dee94b521bf2a18ebf042b78e6f27b
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-04-01 09:13:42 +0100

    Add CLuaInterface hooks

--- HowHaCk6/hooked/CLuaShared.hpp
+++ HowHaCk6/hooked/CLuaShared.hpp
@@ -8,9 +8,15 @@
 #include "../interfaces.hpp"
 
 typedef CLuaInterface* (__thiscall* CreateLuaInterfaceFn)(CLuaShared*, LuaInterfaceType, bool);
+typedef bool(__thiscall* RunStringExFn)(CLuaInterface*, const char*, const char*, const char*, bool, bool, bool, bool);
 
 namespace HowHack {
+    VMTHook* g_pLuaInterfaceHook;
+
 	CreateLuaInterfaceFn g_pOCreateLuaInterface;
+    RunStringExFn g_pORunStringEx;
+
+    void UpdateLuaInterfaceHooks(CLuaInterface* pLuaInterface);
 
     CLuaInterface* __fastcall hkCreateLuaInterfaceFn(CLuaShared* _this,
 #ifndef _WIN64
@@ -24,9 +30,35 @@ namespace HowHack {
         // TODO hook the lua state
 
         HowHack::UpdateLuaInterface(pLuaInterface);
+        HowHack::UpdateLuaInterfaceHooks(pLuaInterface);
 
         return pLuaInterface;
     }
+
+    bool __fastcall hkRunStringEx(CLuaInterface* _this,
+#ifndef _WIN64
+        void*,
+#endif
+        const char* szFilename, const char* szPath, const char* szStringToRun, bool bRun, bool bPrintErrors, bool bDontPushErrors, bool bNoReturns) {
+        // TODO
+        return g_pORunStringEx(_this, szFilename, szPath, szStringToRun, bRun, bPrintErrors, bDontPushErrors, bNoReturns);
+    }
+
+
+    void UpdateLuaInterfaceHooks(CLuaInterface* pLuaInterface) {
+        // TODO potential memory leak from not deleting the old hook
+        if (pLuaInterface == nullptr) return;
+
+        // Create a new hook
+        g_pLuaInterfaceHook = new VMTHook(pLuaInterface);
+    
+        // Hook the functions
+        g_pORunStringEx = (RunStringExFn)g_pLuaInterfaceHook->hookFunction(111, hkRunStringEx);
+
+        // Say what we did
+        LogHook((DWORD)g_pORunStringEx, "CLuaInterface::RunStringEx");
+    }
+
 }
 
 #endif
