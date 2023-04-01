ccc12e6c8a0c7000d8815ebe587e0d7ea3d83908
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2023-04-01 15:47:38 +0100

    Update Color.h

--- HowHaCk6/sourcesdk/tier0/Color.h
+++ HowHaCk6/sourcesdk/tier0/Color.h
@@ -1,105 +1,91 @@
-#pragma once
-#include <cstdint>
+#ifndef COLOR_H
+#define COLOR_H
 
-struct Color
+class Color
 {
-    uint8_t color[4];
-    float fCol[4];
-    int r, g, b, a;
-    bool rainbow;
-
-    Color(uint8_t R, uint8_t G, uint8_t B, uint8_t A) {
-        color[0] = R;
-        color[1] = G;
-        color[2] = B;
-        color[3] = A;
-        r = R; g = G; b = B; a = A;
-        fCol[0] = (float)R/255.f; fCol[1] = (float)G / 255.f; fCol[2] = (float)B / 255.f; fCol[3] = (float)A / 255.f;
-		rainbow = false;
-    }
-    Color(uint8_t R, uint8_t G, uint8_t B) {
-        color[0] = R;
-        color[1] = G;
-        color[2] = B;
-        color[3] = 255;
-        r = R; g = G; b = B; a = 255;
-        fCol[0] = (float)R / 255.f; fCol[1] = (float)G / 255.f; fCol[2] = (float)B / 255.f; fCol[3] = 1.f;
-		rainbow = false;
-    }
-    bool operator == (const Color& other)
-    {
-        return fCol[0] == other.fCol[0] && fCol[1] == other.fCol[1] && fCol[2] == other.fCol[2] && fCol[3] == other.fCol[3];
-    }
-    bool operator != (const Color& other)
-    {
-        return !(*this == other);
-    }
-	static Color FromHSB(float hue, float saturation, float brightness)
+public:
+	// constructors
+	Color()
 	{
-		float h = hue == 1.0f ? 0 : hue * 6.0f;
-		float f = h - (int)h;
-		float p = brightness * (1.0f - saturation);
-		float q = brightness * (1.0f - saturation * f);
-		float t = brightness * (1.0f - (saturation * (1.0f - f)));
-
-		if (h < 1)
-		{
-			return Color(
-				(uint8_t)(brightness * 255),
-				(uint8_t)(t * 255),
-				(uint8_t)(p * 255)
-			);
-		}
-		else if (h < 2)
-		{
-			return Color(
-				(uint8_t)(q * 255),
-				(uint8_t)(brightness * 255),
-				(uint8_t)(p * 255)
-			);
-		}
-		else if (h < 3)
-		{
-			return Color(
-				(uint8_t)(p * 255),
-				(uint8_t)(brightness * 255),
-				(uint8_t)(t * 255)
-			);
-		}
-		else if (h < 4)
-		{
-			return Color(
-				(uint8_t)(p * 255),
-				(uint8_t)(q * 255),
-				(uint8_t)(brightness * 255)
-			);
-		}
-		else if (h < 5)
-		{
-			return Color(
-				(uint8_t)(t * 255),
-				(uint8_t)(p * 255),
-				(uint8_t)(brightness * 255)
-			);
-		}
-		else
-		{
-			return Color(
-				(uint8_t)(brightness * 255),
-				(uint8_t)(p * 255),
-				(uint8_t)(q * 255)
-			);
-		}
+		*((int*)this) = 0;
 	}
-	// wip
-    /*void RandomizeIfNeeded()
-    {
-        if (rainbow)
-        {
-			static float rainbowFl;
-			rainbowFl += 0.005f;
-			if (rainbowFl > 1.f) rainbowFl = 0.f;
-			*this = FromHSB(rainbowFl, 1.f, 1.f);
-        }
-    }*/
+	Color(int _r, int _g, int _b)
+	{
+		SetColor(_r, _g, _b, 0);
+	}
+	Color(int _r, int _g, int _b, int _a)
+	{
+		SetColor(_r, _g, _b, _a);
+	}
+	Color(float* col_arr)
+	{
+		SetColor(col_arr[0] * 255, col_arr[1] * 255, col_arr[2] * 255, col_arr[3] * 255);
+	}
+
+	// set the color
+	// r - red component (0-255)
+	// g - green component (0-255)
+	// b - blue component (0-255)
+	// a - alpha component, controls transparency (0 - transparent, 255 - opaque);
+	void SetColor(int _r, int _g, int _b, int _a = 0)
+	{
+		_color[0] = (unsigned char)_r;
+		_color[1] = (unsigned char)_g;
+		_color[2] = (unsigned char)_b;
+		_color[3] = (unsigned char)_a;
+	}
+
+	void GetColor(int& _r, int& _g, int& _b, int& _a) const
+	{
+		_r = _color[0];
+		_g = _color[1];
+		_b = _color[2];
+		_a = _color[3];
+	}
+	void SetRawColor(int color32)
+	{
+		*((int*)this) = color32;
+	}
+
+	int GetRawColor() const
+	{
+		return *((int*)this);
+	}
+
+	inline int r() const { return _color[0]; }
+	inline int g() const { return _color[1]; }
+	inline int b() const { return _color[2]; }
+	inline int a() const { return _color[3]; }
+
+
+	unsigned char& operator[](int index)
+	{
+		return _color[index];
+	}
+
+	const unsigned char& operator[](int index) const
+	{
+		return _color[index];
+	}
+
+	bool operator == (const Color& rhs) const
+	{
+		return (*((int*)this) == *((int*)&rhs));
+	}
+
+	bool operator != (const Color& rhs) const
+	{
+		return !(operator==(rhs));
+	}
+
+	Color& operator=(const Color& rhs)
+	{
+		SetRawColor(rhs.GetRawColor());
+		return *this;
+	}
+
+public:
+	unsigned char _color[4];
 };
+
+#endif
